eaca0bea808b8d4a6bf6bec7e55fed64e2f821de
prash1610 <tilekarprashanth1610@gmail.com>
2025-04-30 16:43:40 +0530

    Add files via upload

---
+++ StringUtility.java
@@ -0,0 +1,10 @@
+public class StringUtility{
+public static int getLength(String input)
+{
+return input.length();
+}
+public static void main(String [] args)
+{
+System.out.println(StringUtility.getLength("Hello"));
+}
+}
